c76e794f44857bcfa43b4df51fc45d370b66591c
Lim-jooyoung <74720422+mmporong@users.noreply.github.com>
2026-03-13 17:27:24 +0900

    fix: Refactor SVG generation for cat images
    Updated the SVG generation to include a transparent background and adjusted the path for the images. Added xlink:href for image references and modified the path data for better animation.

--- generate_snake.py
+++ generate_snake.py
@@ -1,21 +1,29 @@
 import os
 
-# 1. 파일명 형식 맞춤: 01.png, 02.png ... 20.png
+# 1. 경로 설정
 BASE_URL = "https://raw.githubusercontent.com/mmporong/running-cats/main"
-# zfill(2)를 사용해 1을 01로 만듭니다.
 CAT_IMAGES = [f"{BASE_URL}/{str(i).zfill(2)}.png" for i in range(1, 21)]
 
 def generate_svg():
     width = 800
     height = 150
-    # 경로 설정 (화면 밖에서 들어와서 밖으로 나가는 연출)
-    path_data = "M -100,60 H 900" 
+    # 경로: 화면 왼쪽 밖에서 오른쪽 끝까지 가로지르는 경로
+    path_data = "M -100,75 H 900" 
 
+    # 🌟 핵심 수정: xmlns:xlink 네임스페이스 추가
     svg_header = f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n'
-
-# image 태그 부분도 href 대신 xlink:href 를 써보는 것이 더 확실합니다.
-content += f'''
-  <image xlink:href="{img_url}" width="50" height="50">
+    svg_footer = '</svg>'
+    
+    # 배경 (투명)
+    content = '  <rect width="100%" height="100%" fill="transparent" />\n'
+    
+    for i, img_url in enumerate(CAT_IMAGES):
+        delay = i * 0.6 # 고양이 간격 (필요에 따라 조절)
+        duration = "12s" # 속도 (필요에 따라 조절)
+        
+        # 🌟 핵심 수정: xlink:href 사용
+        content += f'''
+  <image xlink:href="{img_url}" width="50" height="50" y="-25">
     <animateMotion path="{path_data}" dur="{duration}" begin="{delay}s" repeatCount="indefinite" />
   </image>'''
 
@@ -23,5 +31,4 @@ content += f'''
         f.write(svg_header + content + svg_footer)
 
 if __name__ == "__main__":
-
     generate_svg()
